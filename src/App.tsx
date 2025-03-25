e0aeb80d6ef69c2435899046e6118a9ef9f83c03
BrousseWayne <samynedir@gmail.com>
2025-03-25 17:02:01 +0100

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ type Player = { name: "Player 1" | "Player 2"; symbol: "X" | "O" };
 type SquareProps = {
   value: string | null;
   onClick: () => void;
+  disabled: boolean;
 };
 
-function Square({ value, onClick }: SquareProps) {
+function Square({ value, onClick, disabled }: SquareProps) {
   return (
-    <button className="square" onClick={onClick}>
+    <button className="square" onClick={onClick} disabled={disabled}>
       {value}
     </button>
   );
 }
 
 function App() {
-  const [squares, setSquares] = useState<string[]>(Array(BOARD_SIZE).fill(""));
+  const [squares, setSquares] = useState<string[]>(
+    Array(BOARD_SIZE).fill(null)
+  );
   const [currentPlayer, setCurrentPlayer] = useState<number>(0);
 
   const players: Player[] = [
@@ -40,18 +43,28 @@ function App() {
   const handleClick = (index: number) => {
     setSquares((prev) => {
       const newSquares = [...prev];
-      newSquares[index] = players[currentPlayer].symbol;
+      if (!newSquares[index]) {
+        newSquares[index] = players[currentPlayer].symbol;
+      }
       nextTurn();
       return newSquares;
     });
   };
   return (
     <>
-      {squares.map((value, index) => {
-        console.log(value, index);
-        <Square key={index} value={value} onClick={() => handleClick(index)} />;
-      })}
-      {/* <Square value={value} onClick={() => handleClick(index)} />; */}
+      <div className="board">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              key={index}
+              value={value}
+              onClick={() => handleClick(index)}
+              disabled={value !== null}
+            />
+          );
+        })}
+      </div>
+
       <p className="playerTurn">this is: {players[currentPlayer].name}</p>
     </>
   );
