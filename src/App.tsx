adb40d6319330cebdf1926582ab12a4a57deb3ea
BrousseWayne <samynedir@gmail.com>
2025-03-25 13:37:24 +0100

    add enttry point

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import "./App.css";
+
+function Square() {
+  return <button className="square">X</button>;
+}
+
+function App() {
+  return <>{Square()}</>;
+}
+
+export default App;
